201410b70943763b1fb7fdd081beb4750cac27b9
Enea <enea@unlimapps.com>
2018-08-29 09:25:30 -0700

    Limit to WooCommerce only

--- proof-factor.php
+++ proof-factor.php
@@ -30,53 +30,60 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-proof-factor-activator.php
- */
-function activate_proof_factor() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-proof-factor-activator.php';
-	Proof_Factor_Activator::activate();
+    /**
+     * Currently plugin version.
+     * Start at version 1.0.0 and use SemVer - https://semver.org
+     * Rename this for your plugin and update it as you release new versions.
+     */
+    define('PLUGIN_NAME_VERSION', '1.0.0');
+
+    /**
+     * The code that runs during plugin activation.
+     * This action is documented in includes/class-proof-factor-activator.php
+     */
+    function activate_proof_factor()
+    {
+        require_once plugin_dir_path(__FILE__) . 'includes/class-proof-factor-activator.php';
+        Proof_Factor_Activator::activate();
+    }
+
+    /**
+     * The code that runs during plugin deactivation.
+     * This action is documented in includes/class-proof-factor-deactivator.php
+     */
+    function deactivate_proof_factor()
+    {
+        require_once plugin_dir_path(__FILE__) . 'includes/class-proof-factor-deactivator.php';
+        Proof_Factor_Deactivator::deactivate();
+    }
+
+    register_activation_hook(__FILE__, 'activate_proof_factor');
+    register_deactivation_hook(__FILE__, 'deactivate_proof_factor');
+
+    /**
+     * The core plugin class that is used to define internationalization,
+     * admin-specific hooks, and public-facing site hooks.
+     */
+    require plugin_dir_path(__FILE__) . 'includes/class-proof-factor.php';
+
+    /**
+     * Begins execution of the plugin.
+     *
+     * Since everything within the plugin is registered via hooks,
+     * then kicking off the plugin from this point in the file does
+     * not affect the page life cycle.
+     *
+     * @since    1.0.0
+     */
+    function run_proof_factor()
+    {
+
+        $plugin = new Proof_Factor();
+        $plugin->run();
+
+    }
+
+    run_proof_factor();
 }
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-proof-factor-deactivator.php
- */
-function deactivate_proof_factor() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-proof-factor-deactivator.php';
-	Proof_Factor_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_proof_factor' );
-register_deactivation_hook( __FILE__, 'deactivate_proof_factor' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-proof-factor.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_proof_factor() {
-
-	$plugin = new Proof_Factor();
-	$plugin->run();
-
-}
-run_proof_factor();
