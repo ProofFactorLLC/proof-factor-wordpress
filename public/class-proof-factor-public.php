4eb12ce51af542449b8321bdd51d93ddfb71e1d6
Enea <enea@unlimapps.com>
2018-08-29 01:27:59 -0700

    One more config fetch change

--- public/class-proof-factor-public.php
+++ public/class-proof-factor-public.php
@@ -20,102 +20,107 @@
  * @subpackage Proof_Factor/public
  * @author     Proof Factor LLC <enea@prooffactor.com>
  */
-class Proof_Factor_Public {
+class Proof_Factor_Public
+{
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
-	private $plugin_name;
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $plugin_name The ID of this plugin.
+     */
+    private $plugin_name;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $version The current version of this plugin.
+     */
+    private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $plugin_name, $version ) {
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param      string $plugin_name The name of the plugin.
+     * @param      string $version The version of this plugin.
+     */
+    public function __construct($plugin_name, $version)
+    {
 
-		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
 
-	}
+    }
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
+    /**
+     * Register the stylesheets for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles()
+    {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Proof_Factor_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Proof_Factor_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Proof_Factor_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Proof_Factor_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/proof-factor-public.css', array(), $this->version, 'all' );
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/proof-factor-public.css', array(), $this->version, 'all');
 
-	}
+    }
 
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
+    /**
+     * Register the JavaScript for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts()
+    {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Proof_Factor_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Proof_Factor_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Proof_Factor_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Proof_Factor_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/proof-factor-public.js', array( 'jquery' ), $this->version, false );
+        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/proof-factor-public.js', array('jquery'), $this->version, false);
 
-	}
-	
-	public function embed_proof() {
+    }
+
+    public function embed_proof()
+    {
         $options = get_option($this->plugin_name);
-        $proof_account_id = $options['account_id'] ? $options['account_id'] : 'blank';
-		$remote_html_key = "{$this->plugin_name}_remote_plugin_html_{$proof_account_id}";
-		if ( false === ( $html = get_transient( $remote_html_key ) ) ) {
-			$response = wp_remote_get( "https://api.prooffactor.com/v1/partners/woo_commerce/embed?account_id={$proof_account_id}");
-			if ( is_wp_error( $response ) ) {
-				return;
-			}
-			$data = wp_remote_retrieve_body( $response );
-			if ( is_wp_error( $data ) ) {
-				return;
-			}
-			set_transient( $remote_html_key, $data, 24 * HOUR_IN_SECONDS );
-		}
-		echo $html;
-	}
+        $proof_account_id = $options['account_id'];
+        $remote_html_key = "{$this->plugin_name}_remote_plugin_html_{$proof_account_id}";
+        if (false === ($html = get_transient($remote_html_key))) {
+            $response = wp_remote_get("https://api.prooffactor.com/v1/partners/woo_commerce/embed?account_id={$proof_account_id}");
+            if (is_wp_error($response)) {
+                return;
+            }
+            $data = wp_remote_retrieve_body($response);
+            if (is_wp_error($data)) {
+                return;
+            }
+            set_transient($remote_html_key, $data, 24 * HOUR_IN_SECONDS);
+        }
+        echo $html;
+    }
 
 }
